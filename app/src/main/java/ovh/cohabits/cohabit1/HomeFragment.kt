c0b2c2ffd5ce14ad28572d14367b0e6f1a1b31b6
lucielong <lucie.long@epfedu.fr>
2023-05-07 18:43:57 +0200

    update HomeFragment

--- app/src/main/java/ovh/cohabits/cohabit1/HomeFragment.kt
+++ app/src/main/java/ovh/cohabits/cohabit1/HomeFragment.kt
@@ -2,6 +2,7 @@ package ovh.cohabits.cohabit1
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -30,6 +31,7 @@ class HomeFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
@@ -37,8 +39,20 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val cardView = view?.findViewById<CardView>(R.id.card_view_box)
+        cardView?.setBackgroundResource(R.drawable.box)
+        val button_creer = view?.findViewById<Button>(R.id.button_creer_colocation)
+        button_creer?.setBackgroundResource(R.drawable.edit_button)
+        button_creer?.setOnClickListener() {
+            println("test")
+            val intent = Intent(activity, ConnexionActivity::class.java)
+            activity?.startActivity(intent)
+        }
+        val button_rejoindre = view?.findViewById<Button>(R.id.button_rejoindre_colocation)
+        button_rejoindre?.setBackgroundResource(R.drawable.edit_button)
         //Mettre la condition ici
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return view
     }
 
     companion object {
